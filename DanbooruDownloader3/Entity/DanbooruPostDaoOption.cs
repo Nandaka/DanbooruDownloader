7beccd658f910c5a38de4199b576401e3d263145
Nandaka <nchek2000@gmail.com>
2012-10-26 00:10:44 +0800

    Forgot to add DanbooruPostDaoOption.cs

---
+++ DanbooruDownloader3/Entity/DanbooruPostDaoOption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DanbooruDownloader3.Entity
+{
+    public class DanbooruPostDaoOption
+    {
+        public DanbooruProvider Provider { get; set; }
+
+        public string Query { get; set; }
+
+        public string SearchTags { get; set; }
+
+        public string Url { get; set; }
+
+        public string Referer { get; set; }
+
+        public bool IsXML { get; set; }
+
+        public List<DanbooruTag> BlacklistedTags { get; set; }
+
+        public System.Text.RegularExpressions.Regex BlacklistedTagsRegex { get; set; }
+
+        public bool BlacklistedTagsUseRegex { get; set; }
+    }
+}
