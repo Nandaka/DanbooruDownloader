2fe9f788f8dadf3a1a8c11fc4b4f9f0d66083012
Nandaka <nchek2000@gmail.com>
2016-10-02 17:52:56 +0800

    v20161002

--- DanbooruDownloader3/Properties/AssemblyInfo.cs
+++ DanbooruDownloader3/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2016.08.16")]
-[assembly: AssemblyFileVersion("3.2016.08.16")]
+[assembly: AssemblyVersion("3.2016.10.02")]
+[assembly: AssemblyFileVersion("3.2016.10.02")]
