df826bce6830b8b2b690890f4bbd5cfc4017992d
Nandaka <nchek2000@gmail.com>
2018-07-21 11:23:41 +0800

    update changelog

--- DanbooruDownloader3/Properties/AssemblyInfo.cs
+++ DanbooruDownloader3/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nandaka")]
 [assembly: AssemblyProduct("DanbooruDownloader3")]
-[assembly: AssemblyCopyright("Copyright © Nandaka 2016-2017")]
+[assembly: AssemblyCopyright("Copyright © Nandaka 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2017.09.22")]
-[assembly: AssemblyFileVersion("3.2017.09.22")]
+[assembly: AssemblyVersion("3.2018.21.07")]
+[assembly: AssemblyFileVersion("3.2017.21.07")]
