3f4b1242364550aa3076d9afc8d8622929b70d77
Nandaka <nchek2000@gmail.com>
2022-08-04 13:23:23 +0800

    update default provider

--- DanbooruDownloader3/Properties/AssemblyInfo.cs
+++ DanbooruDownloader3/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.2022.08.04")]
-[assembly: AssemblyFileVersion("3.2022.04.06")]
+[assembly: AssemblyFileVersion("3.2022.08.04")]
